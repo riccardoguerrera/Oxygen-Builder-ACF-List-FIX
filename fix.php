3d579d1ff20d3a68528c516618b0c8ec6520f952
Riccardo Guerrera <riccardo.guerrera.pa@gmail.com>
2024-08-07 17:33:29 +0200

    Update fix.php

--- fix.php
+++ fix.php
@@ -1,8 +1,8 @@
 <?php
 
-  function rgwebdev_oxy_acf_fix($dynamic_data) {
+  function dm_rgwebdev_cdb_oxy_acf_fix($dynamic_data) {
   	foreach ($dynamic_data as $index1 => $ddata) {
-  		if (isset($ddata) && !empty($ddata) && $ddata['data'] === 'acf_content') {
+  		if (isset($ddata) && !empty($ddata) && substr($ddata['data'], 0, 4) === 'acf_') {
   			if (is_object($ddata)) continue;
   			foreach ($ddata['properties'] as $index2 => $dd) {
   				if (is_object($dd)) continue;
@@ -16,6 +16,6 @@
   	return($dynamic_data);
   }
   
-  add_filter('oxygen_custom_dynamic_data', 'rgwebdev_oxy_acf_fix', 999, 1);
+  add_filter('oxygen_custom_dynamic_data', 'dm_rgwebdev_cdb_oxy_acf_fix', 999, 1);
 
 ?>
