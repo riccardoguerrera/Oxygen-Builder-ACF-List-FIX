cabd2e8c8c59268ff40debd1777e579612c47c13
Riccardo Guerrera <riccardo.guerrera.pa@gmail.com>
2024-08-07 19:23:48 +0200

    Update fix.php

--- fix.php
+++ fix.php
@@ -1,6 +1,6 @@
 <?php
 
-  function dm_rgwebdev_cdb_oxy_acf_fix($dynamic_data) {
+  function rgwebdev_oxy_acf_fix($dynamic_data) {
   	foreach ($dynamic_data as $index1 => $ddata) {
   		if (isset($ddata) && !empty($ddata) && substr($ddata['data'], 0, 4) === 'acf_') {
   			if (is_object($ddata)) continue;
@@ -16,6 +16,6 @@
   	return($dynamic_data);
   }
   
-  add_filter('oxygen_custom_dynamic_data', 'dm_rgwebdev_cdb_oxy_acf_fix', 999, 1);
+  add_filter('oxygen_custom_dynamic_data', 'rgwebdev_oxy_acf_fix', 999, 1);
 
 ?>
